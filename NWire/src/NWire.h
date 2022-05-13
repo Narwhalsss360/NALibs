9a851272f4fdad7208ae4cf1f8eac0447a8711db
Franklyn Vasquez <franklynvasquez24@gmail.com>
2022-05-12 22:17:36 -0400

    Added reinterpret_c_style, added dependency to NWire, Finished NWire protoypes.

--- NWire/src/NWire.h
+++ NWire/src/NWire.h
@@ -7,4 +7,49 @@
 	#include "WProgram.h"
 #endif
 
+#include "NDefs.h"
+
+#define MAIN_BUFFER_SIZE 5
+#define DATA_SIZE 4
+#define NWD_SIZE sizeof(NWD)
+#define INVALID_NWD(nwd) (nwd.address == 0 && nwd.data == 0) ? true : false
+#define SEARCH_SEND 0x01
+#define SEARCH_RECEIVED 0x02
+
+enum NWireErrors
+{
+    NO_DATA = 1
+};
+
+struct NWireData
+{
+    uint8_t address;
+    uint32_t data;
+};
+
+typedef NWireData NWD;
+typedef NWD* pNWD;
+
+class NWireSlave
+{
+private:
+    mainBuffer[MAIN_BUFFER_SIZE];
+    pNWD receivedData;
+    pNWD sendData;
+    uint8_t length;
+    uint8_t receivedDataIndex;
+    uint8_t sendDataIndex;
+    uint8_t txAddress;
+    void clearMainBuffer();
+    uint32_t bytesToU32() const;
+    uint8_t search(uint8_t, uint8_t);
+public:
+    NWireSlave();
+    NWireSlave(uint8_t);
+    NWD onReceive();
+    void onRequest();
+    bool getData();
+    void add(pNWD);
+    uint8_t getLastError();
+};
 #endif
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # NALibs
 ## Working repository for all NLibs
+# To Do
+## Copy over NDefs.h to all libraries to add reinterpret_c_style
