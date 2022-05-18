2fb36002b3e28c092081d6e556648bc003ac3996
Franklyn Vasquez <franklynvasquez24@gmail.com>
2022-05-18 13:13:02 -0400

    [ADD] Write String to NSerialTest

--- Tests/NSerial Test/Program.cs
+++ Tests/NSerial Test/Program.cs
@@ -1,15 +1,46 @@
 ﻿using System;
 using System.IO.Ports;
+using System.Text;
+
 namespace NSerial_Test
 {
     internal class Program
     {
-        const byte EOT = 0x04;
+        const byte MIN_MSG_SIZE = 5;
+        const byte SOH = 0x01;
         static SerialPort port = new SerialPort();
 
         static void write()
         {
+            Console.Write("Address: ");
+            int address = int.Parse(Console.ReadLine());
 
+            Console.Write("Data:");
+            string dataInput = Console.ReadLine();
+
+            byte[] bytes = new byte[dataInput.Length + MIN_MSG_SIZE - 1];
+            bytes[0] = SOH;
+            bytes[1] = (byte)dataInput.Length;
+
+            byte[] addressBytes = BitConverter.GetBytes(address);
+            bytes[2] = addressBytes[1]; //LSB-First
+            bytes[3] = addressBytes[0]; //LSB-First
+
+            byte[] dataBytes = Encoding.UTF8.GetBytes(dataInput);
+            for (int i = 0; i < dataBytes.Length; i++)
+            {
+                bytes[i + 4] = dataBytes[i];
+            }
+
+            port.Write(bytes, 0, bytes.Length);
+
+            string echo = "";
+            for (int i = 0; i < bytes.Length; i++)
+            {
+                echo += $"0x{bytes[i].ToString("X2")}, ";
+            }
+            Console.Clear();
+            Console.WriteLine($"Sent: {echo}");
         }
 
         static void read()
@@ -29,7 +60,7 @@ namespace NSerial_Test
 
                 byte inChar = (byte)port.ReadByte();
                 
-                if (inChar == EOT && size >= 6)
+                if (size >= MIN_MSG_SIZE)
                     break;
                 
                 output += $"0x{ inChar.ToString("X2") }, ";
@@ -62,6 +93,7 @@ namespace NSerial_Test
                 Console.WriteLine();
             }
 
+            Console.Clear();
             port.Open();
             port.DiscardInBuffer();
             if (rw == "R")
