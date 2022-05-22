d2fb82a20a259eaca647cca6af7b62fa2def3881
Franklyn Vasquez <franklynvasquez24@gmail.com>
2022-05-22 14:45:30 -0400

    [FIX] For ref sketch

--- Reference/Reference Sketch/Debug/Reference Sketch.cpp.d
+++ Reference/Reference Sketch/Debug/Reference Sketch.cpp.d
@@ -11,15 +11,15 @@ C:\Users\sandr\AppData\Local\Temp\VMBuilds\Reference\ Sketch\uno\Debug\Reference
  C:\Users\sandr\AppData\Local\arduino15\packages\arduino\hardware\avr\1.8.5\cores\arduino/USBAPI.h \
  C:\Users\sandr\AppData\Local\arduino15\packages\arduino\hardware\avr\1.8.5\cores\arduino/Arduino.h \
  C:\Users\sandr\AppData\Local\arduino15\packages\arduino\hardware\avr\1.8.5\variants\standard/pins_arduino.h \
- C:\Users\sandr\source\repos\Narwhalsss360\NarsLibs\NALibs\NDefs\src/NDefs.h \
+ C:\Users\sandr\source\repos\Narwhalsss360\NarsLibs\NALibs\NColor\src/NColor.h \
  C:\Users\sandr\AppData\Local\arduino15\packages\arduino\hardware\avr\1.8.5\cores\arduino/Arduino.h \
- C:\Users\sandr\source\repos\Narwhalsss360\NarsLibs\NALibs\NWire\src/NWire.h \
- C:\Users\sandr\AppData\Local\arduino15\packages\arduino\hardware\avr\1.8.5\libraries\Wire\src/Wire.h \
- C:\Users\sandr\AppData\Local\arduino15\packages\arduino\hardware\avr\1.8.5\cores\arduino/Stream.h \
- C:\Users\sandr\source\repos\Narwhalsss360\NarsLibs\NALibs\NTimer\src/NTimer.h \
- C:\Users\sandr\source\repos\Narwhalsss360\NarsLibs\NALibs\NTimer\src/Events.h \
- C:\Users\sandr\source\repos\Narwhalsss360\NarsLibs\NALibs\NTimer\src/TimeLiterals.h \
+ C:\Users\sandr\source\repos\Narwhalsss360\NarsLibs\NALibs\NDefs\src/NDefs.h \
  C:\Users\sandr\source\repos\Narwhalsss360\NarsLibs\NALibs\NFuncs\src/NFuncs.h \
  C:\Users\sandr\source\repos\Narwhalsss360\NarsLibs\NALibs\NPush\src/NPush.h \
  C:\Users\sandr\source\repos\Narwhalsss360\NarsLibs\NALibs\NRotary\src/NRotary.h \
- C:\Users\sandr\source\repos\Narwhalsss360\NarsLibs\NALibs\NColor\src/NColor.h
+ C:\Users\sandr\source\repos\Narwhalsss360\NarsLibs\NALibs\NTimer\src/NTimer.h \
+ C:\Users\sandr\source\repos\Narwhalsss360\NarsLibs\NALibs\NTimer\src/Events.h \
+ C:\Users\sandr\source\repos\Narwhalsss360\NarsLibs\NALibs\NTimer\src/TimeLiterals.h \
+ C:\Users\sandr\source\repos\Narwhalsss360\NarsLibs\NALibs\NWire\src/NWire.h \
+ C:\Users\sandr\AppData\Local\arduino15\packages\arduino\hardware\avr\1.8.5\libraries\Wire\src/Wire.h \
+ C:\Users\sandr\AppData\Local\arduino15\packages\arduino\hardware\avr\1.8.5\cores\arduino/Stream.h
--- Reference/Reference Sketch/Reference Sketch.ino
+++ Reference/Reference Sketch/Reference Sketch.ino
@@ -1,13 +1,11 @@
-#include <NDefs.h>
-#include <NWire.h>
-#include <NTimer.h>
+#include <NColor.h>
 #include <NFuncs.h>
 #include <NPush.h>
 #include <NRotary.h>
-#include <NColor.h>
-#include <NSerialCom.h>
+#include <NTimer.h>
+#include <NWire.h>
 
-void setup()
+void setup() 
 {
 
 }
