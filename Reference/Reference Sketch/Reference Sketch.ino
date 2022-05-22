5d2664ac8bff2a136752f0c22783cdcd4d45fb4d
Franklyn Vasquez <franklynvasquez24@gmail.com>
2022-05-22 14:50:15 -0400

    [START] Reference Sketch

--- Reference/Reference Sketch/Reference Sketch.ino
+++ Reference/Reference Sketch/Reference Sketch.ino
@@ -6,7 +6,19 @@
 #include <NTimer.h>
 #include <NWire.h>
 
-void setup() 
+
+#pragma region NColor
+constexpr byte LED_RED_PIN = 7;
+constexpr byte LED_GREEN_PIN = 8;
+constexpr byte LED_BLUE_PIN = 9;
+RGBA rgba(BYTE_MAX, BYTE_MAX, BYTE_MAX, BYTE_MAX);
+RGB rgb(BYTE_MAX, BYTE_MAX, BYTE_MAX);
+HSV hsv(360, 100, 100);
+Color color;
+RGBLed rgbLed(LED_RED_PIN, LED_GREEN_PIN, LED_BLUE_PIN);
+#pragma endregion
+
+void setup()
 {
 
 }
