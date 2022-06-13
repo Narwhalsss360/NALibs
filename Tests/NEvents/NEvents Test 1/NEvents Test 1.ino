e7315b0335f484211e3010cdbccefa9d6adec7c4
Franklyn Vasquez <franklynvasquez24@gmail.com>
2022-06-12 21:18:42 -0400

    [TEST] With lambdas

--- Tests/NEvents/NEvents Test 1/NEvents Test 1.ino
+++ Tests/NEvents/NEvents Test 1/NEvents Test 1.ino
@@ -2,10 +2,15 @@
 #include <NTimer.h>
 #include "Pin.h"
 
-#define DEBOUNCE 15
+#define NONE
+#define LAMBDA(capture, args, defenition) [capture](args) defenition
+#define SIMPLE_LAMBDA(defenition) []() defenition
+#define EVENT_LAMBDA(defenition) [](EventArgs* args) defenition
+#define DEBOUNCE 25
 
 uint32_t last;
 Pin button = Pin(2, INPUT_PULLUP);
+uint32_t pressCount = ZERO;
 
 EVENT_FUNCTION(fallingEvent, args)
 {
@@ -17,17 +22,12 @@ EVENT_FUNCTION(risingEvent, args)
 	Serial.println("Rising time: " + String(runtime));
 }
 
-EVENT_FUNCTION(changeEvent, args)
-{
-	Serial.println("Change time: " + String(runtime));
-}
-
 void setup()
 {
 	Serial.begin(1000000);
 	button.falling += fallingEvent;
 	button.rising += risingEvent;
-	button.change += changeEvent;
+	button.change += EVENT_LAMBDA({ Serial.println("Change time: " + String(runtime)); });
 }
 
 void loop()
