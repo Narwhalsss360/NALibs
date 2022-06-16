b6c99cb9a48a8bf9f2e11fe72bc1c343e9513ac0
Franklyn Vasquez <franklynvasquez24@gmail.com>
2022-06-16 00:52:48 -0400

    [CHANGE] Installer

--- Tools/Installer/Form1.Designer.cs
+++ Tools/Installer/Form1.Designer.cs
@@ -230,6 +230,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
             this.ClientSize = new System.Drawing.Size(443, 293);
             this.Controls.Add(this.DeselectAllButton);
             this.Controls.Add(this.SelectAllButton);
