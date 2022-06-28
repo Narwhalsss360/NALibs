aa92908bc3d7d563e200e3b4618768ed04bb83e7
Franklyn Vasquez <franklynvasquez24@gmail.com>
2022-06-27 22:52:18 -0400

    [FIX} Ref Sketch

--- Reference/Reference Sketch/Debug/Wire/utility/twi.c.d
+++
@@ -1,6 +0,0 @@
-C:\Users\sandr\AppData\Local\Temp\VMBuilds\Reference\ Sketch\uno\Debug\Wire\utility\twi.c.o: \
- C:\Users\sandr\AppData\Local\arduino15\packages\arduino\hardware\avr\1.8.5\libraries\Wire\src\utility\twi.c \
- C:\Users\sandr\AppData\Local\arduino15\packages\arduino\hardware\avr\1.8.5\cores\arduino/Arduino.h \
- C:\Users\sandr\AppData\Local\arduino15\packages\arduino\hardware\avr\1.8.5\cores\arduino/binary.h \
- C:\Users\sandr\AppData\Local\arduino15\packages\arduino\hardware\avr\1.8.5\variants\standard/pins_arduino.h \
- C:\Users\sandr\AppData\Local\arduino15\packages\arduino\hardware\avr\1.8.5\libraries\Wire\src\utility\twi.h
--- Reference/Reference Sketch/Reference Sketch.ino
+++ Reference/Reference Sketch/Reference Sketch.ino
@@ -1,20 +1,20 @@
-#include <NColor.h>
-#include <NDefs.h>
-#include <NEvents.h>
-#include <NFuncs.h>
-#include <NHCSR04.h>
-#include <NPush.h>
-#include <NRotary.h>
-#include <NSerialCom.h>
-#include <NTimer.h>
-#include <NWire.h>
-
-
+#include "NColor.h"
+#include "NDefs.h"
+#include "NEvents.h"
+#include "NFuncs.h"
+#include "NHCSR04.h"
+#include "NPush.h"
+#include "NRotary.h"
+//#include "NSerialCom.h" //<- Incomplete
+#include "NTimer.h"
+#include "NWire.h"
 
 void setup()
 {
+
 }
 
 void loop()
 {
+  
 }
