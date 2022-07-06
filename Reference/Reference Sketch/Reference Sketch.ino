e6d5c5e11c7d3f51a6d6464a19e17c9ed42046a5
Franklyn Vasquez <franklynvasquez24@gmail.com>
2022-07-06 17:42:05 -0400

    [FIX] Reference Sketch

--- Reference/Reference Sketch/Reference Sketch.ino
+++ Reference/Reference Sketch/Reference Sketch.ino
@@ -1,14 +1,3 @@
-#include "NColor.h"
-#include "NDefs.h"
-#include "NEvents.h"
-#include "NFuncs.h"
-#include "NHCSR04.h"
-#include "NPush.h"
-#include "NRotary.h"
-//#include "NSerialCom.h" //<- Incomplete
-#include "NTimer.h"
-#include "NWire.h"
-
 void setup()
 {
 
@@ -16,5 +5,5 @@ void setup()
 
 void loop()
 {
-  
+
 }
